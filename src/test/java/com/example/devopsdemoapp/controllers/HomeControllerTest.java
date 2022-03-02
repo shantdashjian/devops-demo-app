f96a99af7f2dea15ff7306179805cedfe23ca027
Shant Dashjian <shantgdashjian@gmail.com>
2022-03-02 00:05:17 -0800

    Modify message

--- src/test/java/com/example/devopsdemoapp/controllers/HomeControllerTest.java
+++ src/test/java/com/example/devopsdemoapp/controllers/HomeControllerTest.java
@@ -20,6 +20,6 @@ public class HomeControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("home"))
                 .andExpect(content().string(
-                        containsString("Welcome to Cheetah World")));
+                        containsString("Welcome to Beautiful Cheetah World")));
     }
 }
