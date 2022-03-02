a10678fb6cacb3c3acd9ffb70bd9af385507437d
Shant Dashjian <shantgdashjian@gmail.com>
2022-03-01 23:46:10 -0800

    First commit

---
+++ src/main/java/com/example/devopsdemoapp/DevopsDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopsdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devopsdemoapp/DevopsDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopsdemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
